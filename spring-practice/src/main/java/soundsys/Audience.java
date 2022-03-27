0d339892b650190389db0af69aad26f652d26fb8
Nate <nnnate423@gmail.com>
2022-03-27 17:50:11 +0800

    branch 1 change 1

--- spring-practice/src/main/java/soundsys/Audience.java
+++ spring-practice/src/main/java/soundsys/Audience.java
@@ -11,8 +11,9 @@ public class Audience {
 
     @Before("cdplaylog()")
     public void CDConsoleLog(){
-        System.out.println("--------CD STARTS PLAYING MASTER MARK--------");
-        System.out.println("--------MASTER MARK--------");
+        System.out.println("--------CD STARTS PLAYING MERGED--------");
+        System.out.println("--------MERGED--------");
+
     }
 
     @After("cdplaylog()")
